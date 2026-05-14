3a77fd42e6e9abd9c412ea86c0b5634a36e23ace
kashifniaxi <kashifkhan0516@gmail.com>
2026-05-15 01:41:52 +0500

    prisma setup completed and tested

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@as-integrations/express5";
+import { prismaClient } from "./lib/db.js";
 
 
 async function startServer() {
@@ -13,14 +14,43 @@ const server = new ApolloServer({
     type Query {
       hello: String
       sayHello(name: String!): String
+      getUsers: [User]
+      getUserById(id: ID!): User
+    }
+    type User {
+      id: ID!
+      firstName: String!
+      lastName: String!
+      email: String!
+
+    },
+    type Mutation {
+      createUser(firstName: String!,lastName:String!, email: String!, password: String!): Boolean
     }
   `,
   resolvers: {
     Query: {
       hello: () => "Hello, world!",
       sayHello: (_: any, { name }: { name: string }) => `Hello, ${name}! , how are you doing?`,
+      getUsers: () => prismaClient.user.findMany(),
+      getUserById: (_: any, { id }: { id: string }) => prismaClient.user.findUnique({ where: { id:id } }),
     },
+    Mutation: {
+      createUser: async (_: any, { firstName, lastName, email, password }: { firstName: string, lastName: string, email: string, password: string }) => {
+        await prismaClient.user.create({
+          data: {
+            firstName,
+            lastName,
+            email,
+            password,
+            salt: "random_salt", // In a real application, generate a unique salt for each user
+          },
+        });
+        return true;
+      }
   },
+
+}
 });
 await server.start();
 
