2c2e024c04313bc77a21c8a950eee490a66e5265
kashifniaxi <kashifkhan0516@gmail.com>
2026-06-27 17:47:24 +0500

    made context for loggedin user

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import { expressMiddleware } from "@as-integrations/express5";
 import createGraphqlServer  from "./graphql/index.js";
+import userService from "./services/user.js";
 
 
 
@@ -18,9 +19,28 @@ app.get("/", (req, res) => {
 app.use(express.json());
 
 
-const server = await createGraphqlServer({});
+// const server = await createGraphqlServer({});
 
-app.use("/graphql", expressMiddleware(server));
+app.use(
+  "/graphql",
+  expressMiddleware(await createGraphqlServer({}), {
+    context: async ({ req }) => {
+      const token = req.headers["tokens"];
+
+      if (!token) {
+        return { user: "user not found" };
+      }
+
+      try {
+        const user = userService.decodeUserToken(token as string);
+        return { user };
+      } catch (err) {
+        console.error("Invalid token:", err);
+        return { user: null };
+      }
+    },
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
